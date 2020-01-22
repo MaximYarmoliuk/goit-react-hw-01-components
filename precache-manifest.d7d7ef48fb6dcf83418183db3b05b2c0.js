9ce61d239dc1f90a5f621ef44dbb701e2b98cf6a
Maxim Yarmoliuk <jarmoluk97@gmail.com>
2020-01-22 22:35:56 +0200

    Updates

---
+++ precache-manifest.d7d7ef48fb6dcf83418183db3b05b2c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198edabcf8d75b84712c2f3214641c00",
+    "url": "/github.io/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "bcd4d8d01db8fdb68378",
+    "url": "/github.io/goit-react-hw-01-components/static/css/main.073dc2a7.chunk.css"
+  },
+  {
+    "revision": "6242e99e9b44a23e84f3",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.51503f83.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.51503f83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bcd4d8d01db8fdb68378",
+    "url": "/github.io/goit-react-hw-01-components/static/js/main.44f541f9.chunk.js"
+  },
+  {
+    "revision": "c4f8711fd7068f29b255",
+    "url": "/github.io/goit-react-hw-01-components/static/js/runtime-main.5b026895.js"
+  }
+]);
